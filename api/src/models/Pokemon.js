8f6b0326d30b3ea0c1d25a471cd34c4929d04e43
andrea2326 <andrea.solariroa@hotmail.com>
2021-11-19 15:20:04 -0300

    models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    life: {
+      type: DataTypes.INTERGER
+    },
+    strength: {
+      type: DataTypes.INTERGER
+    },
+    defending: {
+      type: DataTypes.INTERGER
+    },
+    speed: {
+      type: DataTypes.INTERGER
+    },
+    height: {
+      type: DataTypes.INTERGER
+    },
+    weight: {
+      type: DataTypes.INTERGER
+    }
   });
 };
